4a352a9f43848e1f89fa710042375f896e1f06cb
techatin <xuanchunlu@gmail.com>
2018-01-27 20:12:37 +0800

    minor backend tweaks

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from . import app, socketio, connected_user, code_used, room_numbers
 from . import room_losing_condition
-from flask import render_template
+from flask import render_template, session
 from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 from flask_socketio import close_room
 import json
@@ -25,6 +25,11 @@ def create_room():
     return json.dumps(data)
 
 
+@socketio.on('get_username', namespace='/api')
+def get_username():
+    emit('answer_username', {'username': session['username']})
+
+
 @socketio.on('join', namespace='/api')
 def on_join(data):
     print(rooms())
@@ -34,6 +39,8 @@ def on_join(data):
 
     people_inside = connected_user.get(room_code, [])
     num_people = len(people_inside)
+
+    print(session.get('username', []))
 
     if (room_code not in room_numbers):
         emit('error', {"error_message": "Room does not exist yet."})
@@ -47,12 +54,46 @@ def on_join(data):
         emit('error', {"error_message": "Username is already taken."})
         return
 
+    session['username'] = username
+    session['room'] = room_code
+
     join_room(room_code)
     people_inside.append(username)
     connected_user[room_code] = people_inside
     num_people += 1
 
     print('room joined')
+
+    # if (num_people == 2):
+    #     # tells client that the room is ready
+    #     # and sends them the generated emotions
+    #     emotion_a = emotions[random.randint(0, 6)]
+    #     emotion_b = emotions[random.randint(0, 6)]
+    #
+    #     json_data = json.dumps({
+    #         people_inside[0]: emotion_a,
+    #         people_inside[1]: emotion_b
+    #     })
+    #
+    #     losing_condition = {
+    #         people_inside[0]: emotion_b,
+    #         people_inside[1]: emotion_a
+    #     }
+    #
+    #     room_losing_condition[room_code] = losing_condition
+    #
+    #     print(json_data)
+    #     emit('start_game', {'count': 0, 'data': json_data}, room=room_code)
+
+
+@socketio.on('user_ready', namespace='/api')
+def user_ready():
+
+    username = data['username']
+    room_code = int(data['room'])
+
+    people_inside = connected_user.get(room_code, [])
+    num_people = len(people_inside)
 
     if (num_people == 2):
         # tells client that the room is ready
@@ -73,14 +114,14 @@ def on_join(data):
         room_losing_condition[room_code] = losing_condition
 
         print(json_data)
-        emit('my_response', {'count': 0, 'data': json_data}, room=room_code)
+        emit('start_game', {'count': 0, 'data': json_data}, room=room_code)
 
 
 @socketio.on('leave', namespace='/api')
 def on_leave(data):
     print(data)
-    username = data['username']
-    room_code = int(data['room'])
+    username = session['username']
+    room_code = session['room']
     people_inside = connected_user.get(room_code, [])
     num_people = len(people_inside)
 
@@ -113,32 +154,47 @@ def on_leave(data):
 @socketio.on('message', namespace='/api')
 def on_message(data):
     message_data = data['body']
-    room_code = int(data['room'])
+    room_code = session['room']
     people_inside = connected_user.get(room_code, [])
 
+    print(session['username'])
+
     if (len(people_inside) != 2):
-        emit('error', {'error_message': 'The other player has not joined yet!'})
+        emit(
+            'error',
+            {'error_message': 'The other player has not joined yet!'})
         return
 
     print("message!")
     print(room_code)
-    emit('message', {'text': message_data}, room=room_code)
+    emit(
+        'message', {
+            'other_user': session['username'],
+            'text': message_data
+        },
+        room=room_code,
+        include_self=False)
     print("emitted")
 
 
 @socketio.on('emotion', namespace='/api')
 def on_emotion(data):
     emotion_data = data['body']
-    user = data['username']
-    room_code = int(data['room'])
+    user = session['username']
+    room_code = session['room']
     people_inside = connected_user.get(room_code, [])
 
     if (len(people_inside) != 2):
-        emit('error', {'error_message': 'The other player has not joined yet!'})
+        emit(
+            'error',
+            {'error_message': 'The other player has not joined yet!'})
         return
 
     # Check if the emotion detected is the winning condition
     # for the other player
 
     if (emotion_data == room_losing_condition[room_code][user]):
-        print("Game Over")
+        emit(
+            'game_over',
+            {'winner': [i for i in people_inside if i != user][0]},
+            room=room_code)
