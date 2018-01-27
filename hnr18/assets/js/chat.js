3c39143e61571b0ef531fda9360e2125a644a443
techatin <xuanchunlu@gmail.com>
2018-01-27 21:30:45 +0800

    chartoom fixes

--- hnr18/assets/js/chat.js
+++ hnr18/assets/js/chat.js
@@ -1,74 +1,74 @@
-var this_user;
-var other_user;
+var this_user = "default_this_user";
+var other_user = "default_other_user";
 var winning_emotion;
 
 $(document).ready(function(){
 	// Connect to socket
 	var socket = io.connect('http://techatin.pythonanywhere.com/api');
 	socket.emit('get_username');
-	
+
 	// Server returns this user's username
 	socket.on('answer_username', function(msg) {
 		this_user = msg.username;
 		socket.emit('user_ready');
 	});
-	
+
 	// Both users have connected, send winning emotion
 	socket.on('start_game', function(msg) {
 		winning_emotion = msg.data[this_user];
 	});
-	
+
 	socket.on('message', function(msg) {
 		insertChat(msg.other_user, msg.text);
 	});
-});
 
-//-- No use time. It is a javaScript effect.
-function insertChat(who, text, time = 0){
-    var control = "";
-    
-    if (who == this_user) {
-        control = '<div class="message-me>' +
-                        '<h4>' + this_user + '</h4>' +
-						'<p>' + text + '</p>' +
-			        '</div>';
-    }
-    
-    else {
-        control = '<div class="message-other">' +
-                        '<h4>' + other_user + '</h4>' +
-					    '<p>' + text + '</p>' +
-				    '</div>';
-    }
-    
-    setTimeout(
-        function(){                        
-            $("#message-display").append(control);
-        }, time);
-    }
-}
-
-$(document).ready(function(){
-	$("#message-send-button").on("click", function(){
-		var text = $("#message-send-text").val();
+	$("#send-message-button").on("click", function(){
+		var text = $("#send-message-text").val();
+		console.log("Button pressed");
+		console.log(text);
 		if (text !== ""){
-			insertChat("me", text);              
-			$("#message-send-text").val('');
+			insertChat(this_user, text);
+			$("#send-message-text").val('');
 		}
 	});
-	$("#message-send-text").on("keyup", function(e){
-		if (e.which == 13){
+	$("#send-message-text").on("keyup", function(e){
+		if (e.which === 13){
 			var text = $(this).val();
 			if (text !== ""){
-				insertChat("me", text);              
+				insertChat(this_user, text);
 				$(this).val('');
 			}
 		}
 	});
 });
 
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time = 0){
+    var control = "";
+
+    if (who === this_user) {
+        control = '<div class="message-me">' +
+                        '<h4>' + this_user + '</h4>' +
+						'<p>' + text + '</p>' +
+			        '</div>';
+    } else {
+        control = '<div class="message-other">' +
+                        '<h4>' + who + '</h4>' +
+					    '<p>' + text + '</p>' +
+				    '</div>';
+    }
+
+	console.log(who);
+	console.log(text);
+
+    setTimeout(
+        function(){
+            $("#message-display").append(control);
+        }, time);
+}
+
 //-- NOTE: No use time on insertChat.
-insertChat("me", "Lorem", 0);  
+insertChat("me", "Lorem", 0);
 insertChat("you", "Ipsum", 1500);
 insertChat("me", "Dolor", 3500);
 insertChat("you", "Sit", 7000);
